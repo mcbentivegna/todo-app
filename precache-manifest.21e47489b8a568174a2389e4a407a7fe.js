57d1efd3c253e96cdabe8cb7975bf6348d575cf3
Michelle Bentivegna <mcbentivegna@gmail.com>
2020-05-26 09:01:42 -0400

    Updates

---
+++ precache-manifest.21e47489b8a568174a2389e4a407a7fe.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a61a3587947d015afc2595ecf92f859a",
+    "url": "/todo-app/index.html"
+  },
+  {
+    "revision": "88e51d9a3e5799b9a59a",
+    "url": "/todo-app/static/css/main.e9e3cd4a.chunk.css"
+  },
+  {
+    "revision": "5473af58506393f722af",
+    "url": "/todo-app/static/js/2.ef0fe245.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/todo-app/static/js/2.ef0fe245.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "88e51d9a3e5799b9a59a",
+    "url": "/todo-app/static/js/main.e217de50.chunk.js"
+  },
+  {
+    "revision": "017594b70fcddfe941fd",
+    "url": "/todo-app/static/js/runtime-main.94e975c4.js"
+  }
+]);
